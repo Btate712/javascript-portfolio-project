29d5ed6eda304c7b4283a77cfbd22953bbe6db25
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-18 14:26:32 -0500

    Added seed data to seeds.rb

--- js-project-backend/db/seeds.rb
+++ js-project-backend/db/seeds.rb
@@ -6,27 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-bob = User.new
-bob.username = "Bob"
-bob.save
+bob = User.create(username: "Bob")
 
-q1 = Question.new
-q1.stem = "What is 2 + 2?"
-q1.choice_1 = "1"
-q1.choice_2 = "2"
-q1.choice_3 = "3"
-q1.choice_4 = "4"
-q1.correct_choice = 4
+topics = [
+  Topic.new(name: "Math"), Topic.new(name: "Science"), Topic.new(name: "Spanish")]
 
+topics.each { |topic| topic.save }
 
-e = Encounter.new
-e.user = bob
-e.question = q1
-e.save
+questions = [
+  Question.new(stem: "What is 2 + 2?", choice_1: "1", choice_2: "2",
+    choice_3: "3", choice_4: "4", correct_choice: "4",
+    topic: Topic.find_by(name: "Math")),
+  Question.new(stem: "What is 2 + 3", choice_1: "4", choice_2: "5",
+    choice_3: "6", choice_4: "7", correct_choice: "2",
+    topic: Topic.find_by(name: "Math")),
+  Question.new(stem: "What is the symbol for Oxygen?", choice_1: "O",
+     choice_2: "Ox", choice_3: "Og", choice_4: "On", correct_choice: "1",
+     topic: Topic.find_by(name: "Science")),
+  Question.new(stem: "How many meters in a km?", choice_1: "100",
+    choice_2: "1000", choice_3: "10000", choice_4: "100000",
+    correct_choice: "2", topic: Topic.find_by(name: "Science")),
+  Question.new(stem: "How do you say 'large' in Spanish?", choice_1: "largeo",
+    choice_2: "pequeno", choice_3: "grande", choice_4: "biggo",
+    correct_choice: "3", topic: Topic.find_by(name: "Spanish")),
+  Question.new(stem: "What does 'estrella' mean in English?",
+    choice_1: "street", choice_2: "extra", choice_3: "strange",
+    choice_4: "star", correct_choice: "4", topic: Topic.find_by(name: "Math"))]
 
-t = Topic.new
-t.name = "Math"
-t.save
-
-q1.topic = t
-q1.save
+questions.each { |question| question.save }
