9ce4b2e0634cfadc688117e692adfb9115f26185
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-18 15:33:30 -0500

    Added Topic destroy method

--- js-project-backend/app/controllers/topics_controller.rb
+++ js-project-backend/app/controllers/topics_controller.rb
@@ -6,12 +6,22 @@ class TopicsController < ApplicationController
 
   def create
     topic = Topic.new
-    topic.name = params['topic_name']
+    topic.name = params[:id]
     if !Topic.find_by(name: topic.name)
       topic.save
       render json: { message: "success" }
     else
-      render json: { message: "fail" }
+      render json: { message: "fail - topic already exists" }
+    end
+  end
+
+  def destroy
+    topicId = params[:id]
+    if topic = Topic.find(topicId)
+      topic.delete
+      render json: { message: "success" }
+    else
+      render json: { message: "fail - topic not found" }
     end
   end
 end
