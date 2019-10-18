25935d79cf238e80277843cea55413c4f3aa7d8a
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-18 15:43:48 -0500

    Corrected typo in Topic create method

--- js-project-backend/app/controllers/topics_controller.rb
+++ js-project-backend/app/controllers/topics_controller.rb
@@ -6,7 +6,7 @@ class TopicsController < ApplicationController
 
   def create
     topic = Topic.new
-    topic.name = params[:id]
+    topic.name = params[:topic_name]
     if !Topic.find_by(name: topic.name)
       topic.save
       render json: { message: "success" }
