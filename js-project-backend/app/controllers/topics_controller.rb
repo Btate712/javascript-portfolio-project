372cc99cb1778369ad22167a4d3784c7ce048d94
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-18 15:24:05 -0500

    Added Topic create method

--- js-project-backend/app/controllers/topics_controller.rb
+++ js-project-backend/app/controllers/topics_controller.rb
@@ -1,6 +1,17 @@
 class TopicsController < ApplicationController
   def index
     topics = Topic.all
-    render json: topics
+    render json: topics, only: [:id, :name]
+  end
+
+  def create
+    topic = Topic.new
+    topic.name = params['topic_name']
+    if !Topic.find_by(name: topic.name)
+      topic.save
+      render json: { message: "success" }
+    else
+      render json: { message: "fail" }
+    end
   end
 end
