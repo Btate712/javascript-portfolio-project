614261f3ca3ad55a53fea3cfcf53142e3f60d7dc
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2019-10-20 11:37:08 -0500

    Added ActiveRecord Validations for new questions - all components must exist

--- js-project-backend/app/models/question.rb
+++ js-project-backend/app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ApplicationRecord
   has_many :encounters
   has_many :users, through: :encounters
   belongs_to :topic
+
+  validates :stem, :choice_1, :choice_2, :choice_3, :choice_4, :correct_choice, :topic_id, presence: true
 end
